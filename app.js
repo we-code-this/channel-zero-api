200bdf47999f6d5aa73da0cea7c157d5ace18aa0
Mike Gregoire <mgregoire@nvzion.com>
2019-06-29 09:04:23 -0400

    Use fastify-helmet

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-import Fastify from "fastify";
-import fileUpload from "fastify-file-upload";
-import cors from "fastify-cors";
-import pino from "pino";
-import routes from "./routes";
+import Fastify from 'fastify';
+import fileUpload from 'fastify-file-upload';
+import cors from 'fastify-cors';
+import helmet from 'fastify-helmet';
+import pino from 'pino';
+import routes from './routes';
 
-const logging = "silent";
+const logging = 'silent';
 const log = pino({
   level: logging,
   prettyPrint: { colorize: true }
@@ -13,10 +14,14 @@ const log = pino({
 function buildApp() {
   const fastify = Fastify({ logger: log });
 
-  fastify.register(cors, { origin: [process.env.ADMIN_ORIGIN, process.env.PUBLIC_ORIGIN] });
+  fastify.register(helmet);
+
+  fastify.register(cors, {
+    origin: [process.env.ADMIN_ORIGIN, process.env.PUBLIC_ORIGIN]
+  });
   fastify.register(fileUpload, {
     useTempFiles: true,
-    tempFileDir: "/tmp/"
+    tempFileDir: '/tmp/'
   });
 
   routes(fastify);
