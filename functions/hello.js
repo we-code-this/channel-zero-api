51d132bf17a5cc8cc58c9b849e1f4495a4ea08fa
Mike Gregoire <mgregoire@nvzion.com>
2020-07-26 15:20:28 -0400

    Try something

--- functions/hello.js
+++ functions/hello.js
@@ -1,13 +1,30 @@
-const validHost = require('../utils/validHost');
-const responses = require('../messages/responses');
+require('dotenv').config();
+
+const hosts = [
+  process.env.DEV_HOST,
+  process.env.EDGE_HOST,
+  process.env.CLIENT_HOST,
+  process.env.ADMIN_HOST,
+];
+
+const isValid = function (currentHost) {
+  let valid = false;
+  hosts.map(function (host) {
+    if (currentHost === host) {
+      valid = true;
+    }
+  });
+
+  return valid;
+};
 
 exports.handler = function(event, context, callback) {
-  if (validHost(event.headers.host)) {
+  if (isValid(event.headers.host)) {
     callback(null, {
       statusCode: 200,
       body: 'Hello, World!'
     });
   } else {
-    callback(null, responses[404]);
+    callback(null, { statusCode: 404, body: 'That doesn’t exist' });
   }
 };
